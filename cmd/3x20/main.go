a7810f7f8a6a2470b2e32cca78fef2d6bb7d92ed
J0eppp <joep.van.dijk04@gmail.com>
2021-05-27 18:53:57 +0200

    Added a timer print, should work fine now

--- cmd/3x20/main.go
+++ cmd/3x20/main.go
@@ -3,23 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/J0eppp/3x20/pkg/beep"
+	"math"
 	"time"
 )
 
 func main() {
 	fmt.Println("3x20 is now running!")
-	//err := beep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away")
-	////err := beeep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away", "assets/warning.png")
-	//if err != nil {
+	// err := beep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away")
+	// //err := beeep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away", "assets/warning.png")
+	// if err != nil {
 	//	panic(err)
-	//}
+	// }
 	endTime := time.Now().Add(20 * time.Minute)
 	for true {
+		end := endTime.Unix()
+		now := time.Now().Unix()
+		diff := end - now
+		minutes := int(math.Floor(float64(diff / 60)))
+		seconds := diff % 60
+		fmt.Printf("Time left: %d:%d           \r", minutes, seconds)
+		// now := time.Now()
+		// fmt.Printf("Time left: %d:%d  \r", endTime.Minute()-now.Minute()-1, 60+(endTime.Second()-now.Second()))
 		if time.Now().After(endTime) {
 			// 20 minutes have passed
-			fmt.Println("Look away :)")
+			fmt.Printf("Look away :)           \r")
 			err := beep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away")
-			//err := beeep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away", "assets/warning.png")
+			// err := beeep.Alert("3x20", "You have looked for 20 minutes at your screen, now please look away for 20 seconds 20 meters away", "assets/warning.png")
 			if err != nil {
 				panic(err)
 			}
@@ -29,7 +38,7 @@ func main() {
 			for true {
 				if time.Now().After(newEndTime) {
 					err = beep.Warn("3x20", "You can continue working now!")
-					//err = beeep.Notify("3x20", "You can continue working now!", "assets/information.png")
+					// err = beeep.Notify("3x20", "You can continue working now!", "assets/information.png")
 					if err != nil {
 						panic(err)
 					}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/J0eppp/3x20
+
+go 1.16
+
+require github.com/gen2brain/beeep v0.0.0-20200526185328-e9c15c258e28 // indirect
